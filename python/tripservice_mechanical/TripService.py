15ac8b3dae412fad18ddd5ca5f29172029c8087f
Thomas Traude <thomas.traude@codecentric.de>
2022-05-11 13:53:48 +0200

    Revert refactoring

--- python/tripservice_mechanical/TripService.py
+++ python/tripservice_mechanical/TripService.py
@@ -5,7 +5,7 @@ from tripservice_mechanical.UserSession import UserSession
 
 class TripService:
     def get_trips_by_user(self, user):
-        logged_user = self.logged_in_user()
+        logged_user = UserSession.get_instance().get_logged_user()
         is_friend = False
         trip_list = []
         if logged_user:
@@ -18,6 +18,3 @@ class TripService:
             return trip_list
         else:
             raise UserNotLoggedInException()
-
-    def logged_in_user(self):
-        return UserSession.get_instance().get_logged_user()
